32aa57a1a7dbc921b7d2ed6d8f36d4ceba86b92c
Zahraa <zahraahawie@gmail.com>
2022-09-16 14:49:38 +0300

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <nav>
+      <img src="/images/logo1.png" alt="logo1" className="nav--logo" />
+      <h3 className="nav--title">my travel journal.</h3>
+    </nav>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    key: 1,
+    title: "Lake Como",
+    description:
+      "Lake Como  is a destination with a pure beauty, a marvelous nature and breath-taking views, from where you can walk, relax, discover the peace and enjoy the calm of an holiday.",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    coverImg: "roma.png",
+    imageUrl: "ITALY",
+    googleMapsUrl: "https://goo.gl/maps/EUaXNR5BU8C8QDQE6",
+  },
+  {
+    key: 2,
+    title: "Museum of the Future",
+    description:
+      "The Museum of the Future combines elements of exhibition, immersive theatre and themed attraction, inviting you to look beyond the present to the possible.",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
+    coverImg: "dubai.png",
+    imageUrl: "DUBAI",
+    googleMapsUrl: "https://goo.gl/maps/wiLu8jJQk4CDfSyv6",
+  },
+  {
+    key: 3,
+    title: "Lake Geneva",
+    description:
+      "Lake Geneva, Europe's largest Alpine lake, straddles the Swiss/French border, and laps at the shores of some of Switzerland's most popular cities.",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
+    coverImg: "switzerland.png",
+    imageUrl: "SWITZERLAND",
+    googleMapsUrl: "https://goo.gl/maps/N63KjUBF39TnpSJZA",
+  },
+];
+
+export default data;
